df3054f0011cd8b6019f712cf2d6f56610a5a0a8
wenqingxu615 <516770005@qq.com>
2021-04-26 17:59:18 -0400

    add header to env

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 require('dotenv').config('./env')
 const uri = process.env.MONGODB_URI
+const header = process.env.REACT_HEADER
 
 const session = require('express-session')
 app.use(session({
@@ -21,7 +22,7 @@ mongoose.connect(
 
 // Configures CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', header);
     res.header('Access-Control-Allow-Headers',
         'Content-Type, X-Requested-With, Origin');
     res.header('Access-Control-Allow-Methods',
